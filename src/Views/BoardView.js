84b1b4e308ab0f43f95bd78e58f07e6ef87cdbcf
shamseen <shamseen.c@gmail.com>
2021-09-27 23:39:23 -0400

    basic react-flow example

--- src/Views/BoardView.js
+++ src/Views/BoardView.js
@@ -1,9 +1,20 @@
 import React from "react";
+import ReactFlow from "react-flow-renderer";
+import nodeElements from "../DataServices/MockNodeData";
+import "../styles/boardView.scss";
+
+// setting board size
+// src https://reactflow.dev/examples/
+const onLoad = (reactFlowInstance) => {
+  console.log("flow loaded:", reactFlowInstance);
+  reactFlowInstance.fitView();
+};
 
 export default function Boardview() {
   return (
     <div id="board-view">
       <h1>Research-storyboard</h1>
+      <ReactFlow elements={nodeElements} onLoad={onLoad} />
     </div>
   );
 }
