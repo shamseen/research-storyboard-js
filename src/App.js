806fd68740ecffd7f49271f05c7a3827c5246b10
shamseen <shamseen.c@gmail.com>
2021-09-27 23:58:27 -0400

    basic add node button (no editing)

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import React from "react";
 import Homepage from "./Views/Homepage";
 import "./styles/App.scss";
 
--- src/Views/BoardView.js
+++ src/Views/BoardView.js
@@ -1,6 +1,6 @@
-import React from "react";
+import { useState, useCallback } from "react";
 import ReactFlow from "react-flow-renderer";
-import nodeElements from "../DataServices/MockNodeData";
+import initialEls from "../DataServices/MockNodeData";
 import "../styles/boardView.scss";
 
 // setting board size
@@ -11,10 +11,29 @@ const onLoad = (reactFlowInstance) => {
 };
 
 export default function Boardview() {
+  const [elements, setElements] = useState(initialEls);
+
+  // "add" btn functionality
+  // src: https://reactflow.dev/examples/save-and-restore/
+  const setNodeId = () => `randomnode_${+new Date()}`;
+
+  const addNode = useCallback(() => {
+    const newNode = {
+      id: setNodeId(),
+      data: { label: "Added node" },
+      position: {
+        x: Math.random() * window.innerWidth - 100,
+        y: Math.random() * window.innerHeight,
+      },
+    };
+    setElements((els) => els.concat(newNode));
+  }, [setElements]);
+
   return (
     <div id="board-view">
       <h1>Research-storyboard</h1>
-      <ReactFlow elements={nodeElements} onLoad={onLoad} />
+      <button onClick={addNode}>Add Node</button>
+      <ReactFlow elements={elements} onLoad={onLoad} />
     </div>
   );
 }
